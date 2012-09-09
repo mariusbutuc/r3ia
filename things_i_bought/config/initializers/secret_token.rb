577bbe3a9d3a3223c4d621a5774878a2c20636c8
Marius Butuc <marius.butuc@gmail.com>
2012-09-08 22:04:27 -0400

    Add Purchases scaffold

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '10c9706b1b32d450b19d106cf489cbde36336ea2e24bf5e4751b7985d91936671a95c93c0e6369b9558a4e120d3048dbf8b25b0de460be3ea6753aefde55642a'
